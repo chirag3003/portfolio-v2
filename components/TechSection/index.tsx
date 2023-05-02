8ed9793abdf39166ddda413a4f42f34a727a0f07
Chirag Bhalotia <me@chirag.codes>
2023-05-02 14:59:04 +0530

    fixed some responsive issues

--- components/TechSection/index.tsx
+++ components/TechSection/index.tsx
@@ -25,15 +25,15 @@ function TechSection() {
     }, [])
     return (
         <TechSectionStyle id={"tech_section"} className={"flex flex-col lg:flex-row p-10 lg:p-48 pt-36 lg:pt-48"}>
-            <div className="header w-full h-full flex flex-col justify-center">
-                <h2 className={"text-3xl lg:max-w-md my-4 z-10"}>What are my Programming Skills</h2>
-                <p className={"lg:max-w-lg"}>
+            <div className="header w-full lg:h-full p-2 flex flex-col justify-center">
+                <h2 className={"text-3xl lg:text-4xl lg:max-w-md my-4 z-10"}>What are my Programming Skills</h2>
+                <p className={"lg:max-w-lg lg:text-lg"}>
                     I develop simple, intuitive and responsive user interface that helps users get things done with less
                     effort and time with those technologies.
                 </p>
             </div>
-            <div className="skill-list w-full h-full flex flex-col justify-center">
-                <div className="list grid grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 w-full">
+            <div className="skill-list w-full lg:h-full flex flex-col justify-center">
+                <div className="list grid grid-cols-4 p-2 md:grid-cols-5 xl:grid-cols-6 gap-5 w-full">
                     {Array(10).fill("").map((_, index) => {
                         return (
                             <div key={index} className="skill ">
